6ed7e71636c44a0291aa706dfabc3f88e40d2249
JamesORourke <geekjamesorourkejr@icloud.com>
2017-09-09 21:06:52 -0700

    fixed bugs, sorted output.

--- CallCenter.py
+++ CallCenter.py
@@ -1,42 +1,5 @@
 # Assignment: Call Center
-# You're creating a program for a call center. Every time a call comes in you
-# need a way to track that call. One of your program's requirements is to store
-# calls in a queue while callers wait to speak with a call center employee.
-#
-# You will create two classes. One class should be Call, the other CallCenter.
-#
-# Call Class
-# Create your call class with an init method. Each instance of Call() should have:
-#     Attributes:
-#         unique id
-#         caller name
-#         caller phone number
-#         time of call
-#         reason for call
-#     Methods:
-#         display: that prints all Call attributes.
-#
-# CallCenter Class
-# Create your call center class with an init method. Each instance of CallCenter()
-# should have the following attributes:
-#     Attributes:
-#         calls: should be a list of call objects
-#         queue size: should be the length of the call list
-#     Methods:
-#         add: adds a new call to the end of the call list
-#         remove: removes the call from the beginning of the list (index 0).
-#         info: prints the name and phone number for each call in the queue as well as the length of the queue.
-#
-# You should be able to test your code to prove that it works. Remember to build
-# one piece at a time and test as you go for easier debugging!
-#
-# Ninja Level: add a method to call center class that can find and remove a call
-# from the queue according to the phone number of the caller.
-#
-# Hacker Level: If everything is working properly, your queue should be sorted
-# by time, but what if your calls get out of order? Add a method to the call
-# center class that sorts the calls in the queue according to time of call in
-# ascending order.
+
 
 class Call:
     def __init__(self, uniqueId, callerName, callerPhoneNumber, timeOfCall, reasonForCall):
@@ -62,25 +25,25 @@ class CallCenter:
         self.queueSize = 0
 
     def add(self, newCall):
-        self.calls.append(newCall)
         self.queueSize += 1
+        for index in range(0, len(self.calls)):
+            if self.calls[index].timeOfCall > newCall.timeOfCall:
+                self.calls.insert(index, newCall)
+                return self
+        self.calls.append(newCall)
         return self
 
     def find(self, phone):
-        index = 0;
-        for aCall in self.calls:
-            if (aCall.callerPhoneNumber == phone):
+        for index in range(0,len(self.calls)):
+            if (self.calls[index].callerPhoneNumber == phone):
                 return index
-            else:
-                pass;
-            index += 1
         return None
 
     def info (self):
-        print "Call Center information:"
+        print "  -- Call Center information: --"
         for aCall in self.calls:
             aCall.display()
-            print "-----------------------"
+        print "  --------------------------"
         print "There are {0} call(s) in the queue.".format(self.queueSize)
         return self
 
@@ -98,17 +61,16 @@ class CallCenter:
         else:
             return None
 
-    # def sortByDate(self):
-    #     this.calls.sorted(key=lambda, key=attrgetter('timeOfCall'))
-    #     return self;
-
 newcall1 = Call(123,"Abe Ache", "(425) 555-1111", "December 31, 1999 23:59:59.000000", "Drunken 'happy..  new.. yarg...'" )
-newcall2 = Call(123,"Brownie Blown", "(425) 555-2222", "January 1, 12000 09:00:00.000000", "Abe is passed out on my deck." )
-newcall3 = Call(123,"Chuck Clucker", "(425) 555-3333", "December 30, 1999 10:00:00.000000", "Need new years cocktails for Abe." )
-newcall4 = Call(123,"Duke Dracko", "(425) 555-4444", "January 01, 2000 12:00:00.000000", "See, I told you the Y2k thing was a hoax." )
+newcall2 = Call(124,"Brownie Blown", "(425) 555-2222", "January 1, 12000 09:00:00.000000", "Abe is passed out on my deck." )
+newcall3 = Call(125,"Chuck Clucker", "(425) 555-3333", "December 30, 1999 10:00:00.000000", "Need new years cocktails for Abe." )
+newcall4 = Call(126,"Duke Dracko", "(425) 555-4444", "January 01, 2000 12:00:00.000000", "See, I told you the Y2k thing was a hoax." )
+print ("Add to call center with 4 calls.")
 cc = CallCenter().add(newcall1).add(newcall2).add(newcall3).add(newcall4).info()
 
-todo = cc.remove()
+removedEntry = cc.remove()  #defaults to position zero..
+print "remove first entry : ", removedEntry.callerName
 cc.info()
-todo = cc.removeByPhoneNumber("(425) 555-3333")
+todo = cc.removeByPhoneNumber("(425) 555-2222")
+print "Remove a call by the phone number.  (425) 555-2222  -->", todo.callerName
 cc.info()
